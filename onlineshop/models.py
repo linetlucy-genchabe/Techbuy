aef07e2507a1a756f694098284afad2037e94449
linetlucy-genchabe <linetlucy21@gmail.com>
2025-03-26 21:37:11 +0300

    reviews model

--- onlineshop/models.py
+++ onlineshop/models.py
@@ -41,7 +41,7 @@ class Profile(models.Model):
 
 class Products(models.Model):
     Name = models.CharField(max_length=255)
-    Description = models.TextField(max_length=1000)
+    Description = models.TextField()
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Pic = models.ImageField(upload_to= "..\images", )
     product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -76,3 +76,21 @@ class Customers(models.Model):
     def delete_customers(self):
         self.delete()
 
+class Reviews(models.Model):
+    
+    Comment = models.CharField(max_length=1000)
+    rating = models.IntegerField(default=0)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    
+    
+    def save_reviews(self):
+        self.save()
+    
+    def delete_reviews(self):
+        self.delete()
+        
+    @classmethod
+    def get_allreviews(cls):
+        reviews = cls.objects.all()
+        return reviews
