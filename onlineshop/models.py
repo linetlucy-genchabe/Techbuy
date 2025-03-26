a9bba8ac4c3a20852e3f91d977a166cac6c53ac5
linetlucy-genchabe <linetlucy21@gmail.com>
2025-03-26 21:19:10 +0300

    definition of models

--- onlineshop/models.py
+++ onlineshop/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, Http404
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    
+    
+    def __str__(self):
+        return self.name
+
+
+    def save_category(self):
+        self.save()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+   
+    # description = models.TextField(blank=True)
+
+    def save_profile(self):
+        self.save()
+        
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return str(self.user)
+    
+    # def __str__(self):
+    #     return f"{self.user}, {self.bio}, {self.photo}"
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+
+class Products(models.Model):
+    Name = models.CharField(max_length=255)
+    Description = models.TextField(max_length=1000)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Pic = models.ImageField(upload_to= "..\images", )
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    
+    def save_products(self):
+        self.save()
+    
+    def delete_products(self):
+        self.delete()
+        
+    @classmethod
+    def get_allproducts(cls):
+        products = cls.objects.all()
+        return products
+    
+
+class Orders(models.Model):
+
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+
+
+class Customers(models.Model):
+    Name = models.CharField(max_length=255)
+    Email = models.EmailField(unique=True, blank=False, null=False)
+    Phonenumber = models.CharField(max_length=15,blank=False , null=False)
+    location = models.CharField(max_length=200)
+    
+    def save_customers(self):
+        self.save()
+    
+    def delete_customers(self):
+        self.delete()
+
