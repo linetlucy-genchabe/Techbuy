cb4cbebbf8e6063a64fe9f9b497279fee51b5aa1
Reuben-Kipkemboi <rotichkipkemboireuben@gmail.com>
2025-03-27 07:13:07 +0300

    ft:products - register model, define view

--- onlineshop/views.py
+++ onlineshop/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import get_object_or_404, render,redirect
 from django.conf import settings
+from .models import *
 
 
 def index(request):
     
     return render(request, 'index.html')
+
+
+
+def products(request):
+    
+    products = Products.objects.all
+    
+    return render(request, 'public/products.html', {'products':products})
