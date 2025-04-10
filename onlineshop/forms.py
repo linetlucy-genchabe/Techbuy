0e949b1129c74d42fad620e09e3d622003f8e9a1
linetlucy-genchabe <linetlucy21@gmail.com>
2025-04-10 12:30:28 +0300

    ft:adding new review

--- onlineshop/forms.py
+++ onlineshop/forms.py
@@ -4,13 +4,36 @@ from .views import *
 from django.contrib.auth.models import User
 
 
+# class AddReviewsForm(forms.ModelForm):
+#     class Meta:
+#         model = Reviews
+#         fields = ['Comment', 'rating', 'product', 'customer']  # Include all necessary fields
+#         widgets = {
+#             'Comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Write your review...'}),
+#             'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),
+#             'product': forms.Select(attrs={'class': 'form-control'}),  # Dropdown for products
+#             'customer': forms.Select(attrs={'class': 'form-control'}),  # Dropdown for customers
+#         }
+
 class AddReviewsForm(forms.ModelForm):
     class Meta:
         model = Reviews
-        fields = ['Comment', 'rating', 'product', 'customer']  # Include all necessary fields
+        fields = ['Comment', 'rating', 'product', 'customer']
         widgets = {
-            'Comment': forms.Textarea(attrs={'class': 'form-control', 'rows': 3, 'placeholder': 'Write your review...'}),
-            'rating': forms.NumberInput(attrs={'class': 'form-control', 'min': 1, 'max': 5}),
-            'product': forms.Select(attrs={'class': 'form-control'}),  # Dropdown for products
-            'customer': forms.Select(attrs={'class': 'form-control'}),  # Dropdown for customers
+            'Comment': forms.Textarea(attrs={
+                'class': 'form-control custom-style',
+                'rows': 3,
+                'placeholder': 'Write your review...'
+            }),
+            'rating': forms.NumberInput(attrs={
+                'class': 'form-control custom-style',
+                'min': 1,
+                'max': 5
+            }),
+            'product': forms.Select(attrs={
+                'class': 'form-control custom-style'
+            }),
+            'customer': forms.Select(attrs={
+                'class': 'form-control custom-style'
+            }),
         }
